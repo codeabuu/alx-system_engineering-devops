f00f42829be9012b46c19d40f71af8be55f46dc3
codeabuu <abdulngui718@gmail.com>
2023-11-07 04:33:16 -0800

    top ten posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "YourUserAgent"
+        }
+    response = get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            posts = data['data']['children']
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+        else:
+            print("No posts found.")
+    else:
+        print(None)
