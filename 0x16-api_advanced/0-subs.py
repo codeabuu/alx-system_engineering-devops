241704515d742b3f2caa1406a0c0b6f5d0e7de3f
codeabuu <abdulngui718@gmail.com>
2023-11-07 04:22:11 -0800

    updates

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "YourUserAgent"
+        }
+    response = get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
